bfd40468624658aa03e6b927d8e068b04485d8e4
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-04-30 21:45:39 -0500

    Actualizado

---
+++ Modulo 2/CLASE_10/dom01-visualizacion.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    console.log(document.head);      //muestra <head>
+    console.log(document.children);  // muestra [html]
+    console.log(document.title)
+})
---
+++ Modulo 2/CLASE_10/dom05-eventos.js
@@ -0,0 +1,33 @@
+//Eventos addEventListener
+
+function mostrarAlerta() {
+    alert('Este botón usa un evento en línea.');
+}
+
+function agregarProducto() {
+    const ul = document.getElementById
+    ('listaProductos1');
+    const li = document.createElement('li');
+    li.textContent = 'Producto agregado desde un evento inline';
+    ul.appendChild(li);
+}
+
+//Eventos addEventListener
+document.getElementById('boton').addEventListener
+('click', () =>{
+    alert('¡Click detectado');
+});
+
+document.getElementById('campo').addEventListener
+('input', (e) => {
+    console.log('Escribiendo:', e.target.value);
+});
+
+document.getElementById('btnAgregar').addEventListener
+('click', () => {
+    const ul = document.getElementById
+    ('listaProductos2');
+    const li = document.createElement('li');
+    li.textContent = 'Item agregado desde un evento con addEventListener';
+    ul.appendChild(li);
+});
